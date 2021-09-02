2fb77a712657d1e87a09083a8b74016bad302e22
arifrozex <jancokkaw@gmail.com>
2021-09-02 11:34:13 +0700

    first commit

---
+++ handlers/force_sub_handler.py
@@ -0,0 +1,44 @@
+# (c) @Damantha_Jasinghe
+
+from configs import Config
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+async def handle_force_sub(bot, cmd):
+    invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+    try:
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=cmd.from_user.id,
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/arifrozex21).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return 400
+    except UserNotParticipant:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="**Silahkan join dulu ke chaneel!**\n\nAnda tidak dapat akses sebelum join!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🤖 JOIN KLIK SINI", url=invite_link.invite_link)
+                    ],
+                    [
+                        InlineKeyboardButton("🔄 MEMUAT ULANG 🔄", callback_data="refreshmeh")
+                    ]
+                ]
+            ),
+            parse_mode="markdown"
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Something went Wrong. Contact my [Support Group](https://t.me/arifrozex21).",
+            parse_mode="markdown",
+            disable_web_page_preview=True
+        )
+        return 400
